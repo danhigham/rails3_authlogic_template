043a4aab6880a5360b97f5bb358ddef9b9436474
Daniel Higham <danhigham@Daniel-Highams-MacBook-Pro.local>
2011-02-14 14:11:33 +0000

    Initial version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twitterbot::Application.config.secret_token = 'a049a5b7580503f55e4c674e2d04d034a9e47cf9079eb237eb9e844ea564961afd4c93903739464773e95e3d1f86d07d0159ecc888782d869df1b604d61f4ecb'
